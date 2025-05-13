cd51b379ad31e3369d5b1759e6be3f825b287f19
Sandro1M <adin1macharashvili@gmail.com>
2025-05-13 07:01:05 -0700

    commit

---
+++ Day 012/home work/lesson1.py
@@ -0,0 +1,19 @@
+for i in range(30):
+    print("თქვენი სახელი და გვარი")  
+
+for i in range(0, 51):
+    print(i)
+
+for i in range(25, 101):
+    print(i)
+
+for i in range(5, 66, 5):
+    print(i)
+
+for i in range(0, 1001, 150):
+    print(i)
+
+car = "Toyota"  
+for letter in car:
+    print(letter)
+# good work!
