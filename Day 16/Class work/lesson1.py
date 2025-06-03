26a9ebe5d0ae3c8d968efcd96963aa7b708289c6
Sandro1M <adin1macharashvili@gmail.com>
2025-06-03 12:53:43 -0700

    second commit

---
+++ Day 16/Class work/lesson1.py
@@ -0,0 +1,33 @@
+# num = 1
+# while num <= 20:
+#     print(num)
+#     num += 1
+
+
+
+# correct_pin = "1234"
+# user_pin = input("შეიყვანე პინ კოდი: ")
+
+# while user_pin != correct_pin:
+#     print("არასწორი პინ კოდი, სცადე ისევ.")
+#     user_pin = input("შეიყვანე პინ კოდი: ")
+
+# print("პინ კოდი სწორია!")
+
+
+
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+# for i in range(1, 31):
+#     if i % 2 != 0:
+#         print(i)
+
+
+# i = 1
+# while i <= 10:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
---
+++ Day 16/Home work/lesson.py
@@ -0,0 +1,35 @@
+# 1) შემოყვანილი რიცხვის ლუწ/კენტობა
+number = int(input("შეიყვანეთ რიცხვი: "))
+if number % 2 == 0:
+    print("even")  # ლუწია
+else:
+    print("odd")   # კენტია
+
+# 2
+my_name = "გიორგი"
+user_name = input("შეიყვანეთ თქვენი სახელი: ")
+if user_name == my_name:
+    print("Hello")
+else:
+    print("Bye")
+
+# 3)
+score = int(input("შეიყვანეთ თქვენი ქულა: "))
+if score > 90:
+    print("A")
+elif score > 70:
+    print("B")
+elif score > 50:
+    print("C")
+else:
+    print("D")
+
+# 4) 1-დან 100-მდე რიცხვების ლუწ/კენტობა while ციკლით (უიმე {}):
+
+i = 1
+while i <= 100:
+    if i % 2 == 0:
+        print(i, "ლუწია")
+    else:
+        print(i, "კენტია")
+    i += 1
